731ea2af13b193a656c66e69b91a9b3aeeef5f58
s3gm3nt4ti0nf4ult <s3gm3nt4ti0nf4ult@users.noreply.github.com>
2020-12-08 22:03:11 +0100

    d3 done

---
+++ aocode/d3/d3.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+from math import prod
+
+mapa = open('./input.txt').read().split('\n')
+print(len(mapa[0]))
+
+
+def slope(r, d):
+    x, y = 0, 0
+    tree = 0
+    while y < len(mapa) - 1:
+        x += r
+        y += d
+        if mapa[y][x % len(mapa[0])] == '#':
+            tree += 1
+
+    return tree
+
+
+print(f"First: {slope(3, 1)}")
+print(f"Second: {prod([slope(1, 1), slope(3, 1), slope(5, 1), slope(7, 1), slope(1, 2)]) }")
