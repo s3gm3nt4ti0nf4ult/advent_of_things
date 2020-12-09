85779efe68bcef33f49edaa824cc626b49d7d833
s3gm3nt4ti0nf4ult <s3gm3nt4ti0nf4ult@users.noreply.github.com>
2020-12-09 17:59:10 +0100

    day 9

--- aocode/d9/d9.py
+++ aocode/d9/d9.py
@@ -1,5 +1,28 @@
 #! /usr/bin/env python3
+from itertools import combinations
+
 
 nums = [int(x) for x in open('./input.txt').read().split('\n')]
-for l, u in zip(range(0, len(nums) - 25), range(25, len(nums))):
-    
+candaidates = nums[:]
+for l, u in zip(range(0, len(nums) - 26), range(25, len(nums) - 1)):
+    for i in combinations(nums[l:u + 1], 2):
+        if sum(i) == nums[u + 1]:
+            candaidates[u + 1] = None
+            break
+
+candaidates = sorted([x for x in candaidates if x][26:])[0]
+print(f"First: {candaidates}")
+
+nums = nums[:nums.index(candaidates)]
+i, j = 0, 0
+while j < len(nums) - 1:
+    if i >= j:
+        j += 1
+    if sum(nums[i:j + 1]) > candaidates:
+        i += 1
+    elif sum(nums[i:j + 1]) < candaidates:
+        j += 1
+
+    else:
+        print(f"Second: {sorted(nums[i:j+1])[0] + sorted(nums[i:j+1])[-1]}")
+        break
